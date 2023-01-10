4501db61956616798f37ecea7dd55ee36adebea9
Daniel D. Bax <Dan.D.Bax@gmail.com>
2023-01-10 08:09:20 -0600

    initial commit w/ question bank.

---
+++ questions.py
@@ -0,0 +1,12 @@
+question_bank = [
+    ("Jan says that men love the scent of one of the candles she makes. What's it called?","Bonfire"),
+    ("It's the booze cruise. What Gilligan's Island character does Michael tell Pam she is?", "Mary Ann"),
+    ("Name one of the three things that Oscar says DMI, Dunder Mifflin's stock symbol, stands for.", "dummies, morons, idiots"),
+    ("Dwight dressed up for Casino Night in an heirloom tux. What's the story behind it?","His grandfather was buried in it."),
+    ("Someone tells Pam that they'll give her $100 now as a wedding present, or $5,000 in a year. Who's that?", "Ryan"),
+    ("Which office worker gets their company bathrobe taken away?", "Toby"),
+    ("Ryan's assistant tells him he has a call from Eddie Murphy but it's Michael doing an impression of what movie character?", "Donkey, from Shrek"),
+    ("Michael realizes that Dwight doesn't want to be disturbed in his hotel room when he sees this tied to the door knob.", "Dwight's sock"),
+]
+
+print(question_bank[0][0])
