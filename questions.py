1bd1aa96bda99f637de903c70a87b81132ae4497
ddbff9 <50721844+ddbff9@users.noreply.github.com>
2023-01-18 12:07:46 -0600

    Update questions.py
    add 1/18/23 question and fix typos.

--- questions.py
+++ questions.py
@@ -7,10 +7,14 @@ question_bank = [
     ("Which office worker gets their company bathrobe taken away?", "Toby"),
     ("Ryan's assistant tells him he has a call from Eddie Murphy but it's Michael doing an impression of what movie character?", "Donkey, from Shrek"),
     ("Michael realizes that Dwight doesn't want to be disturbed in his hotel room when he sees this tied to the door knob.", "Dwight's sock"),
-    ("Pam and Jim play a prank on Andy that results in A?nyd punching a hole through a wall. What was the prank?", "Jim put Andy's cell phone in the ceiling."),
+    ("Pam and Jim play a prank on Andy that results in Andy punching a hole through a wall. What was the prank?", "Jim put Andy's cell phone in the ceiling."),
     ("They're really going to the hospital but instead, Jim tells Dwight they're going to this restaurant/entertainment place.", "Chuck E. Cheese"),
     ("Angela wants this ailment to be covered by the new health plan.", "dermatitis"),
-    ("What is the name of the student at the job fiar that Michael first insults, then tries to win over?", ("Justin (Polznik)")),
+    ("What is the name of the student at the job fair that Michael first insults, then tries to win over?", "Justin (Polznik)"),
+    ("Michael's gossip about Dwight concerns store-bought varieties of this. What is it?", "manure"),
+    ("What gift did Stanley buy for Pam and Jim's wedding?", "a toaster"),
+    ("Andy tells Angela that one day they'll move here and ''leave this all behind.'' Where do he and Angela want to go?", "Disney's Celebration Village"),
+    ("Michael says that this green toy has a better body than Jim.", "Gumby")
 ]
 
 print(f"Question: {question_bank[0][0]}")
